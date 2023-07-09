038816316dc175b7c24c10d8f594df5cfb07c587
Milena <bojanpavlovicoffice@gmail.com>
2023-07-09 13:31:02 +0200

    remove tag

--- src/test/java/runners/Runner.java
+++ src/test/java/runners/Runner.java
@@ -9,7 +9,6 @@ import org.testng.annotations.Test;
 @CucumberOptions(
         features = "src/test/java/features",
         glue = "steps",
-        tags = "(@Smoke or @Regression)",
         plugin = {"io.qameta.allure.cucumber6jvm.AllureCucumber6Jvm"}
 )
 public class Runner extends AbstractTestNGCucumberTests {
